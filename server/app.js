3874f1ebd29ef8c1c44639d7e5fd39d3c830658e
MatthiasEisenhut <eisenhu.m03@htlwienwest.at>
2021-11-29 17:26:40 +0100

    29.11.2021, 17:26

--- server/app.js
+++ server/app.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const path = require('path');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+const API_KEY = process.env.API_KEY;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server running on Port ${PORT}...`));
+
+console.log('Server started');
