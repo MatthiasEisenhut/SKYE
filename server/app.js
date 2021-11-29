2d1bc9bbb208c09159a815db8f9cc1a9abce4862
MatthiasEisenhut <eisenhu.m03@htlwienwest.at>
2021-11-29 17:56:42 +0100

    29.11.2021, 17:56

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 
 require('dotenv').config();
 
@@ -7,6 +8,17 @@ const PORT = process.env.PORT || 5000;
 const API_KEY = process.env.API_KEY;
 
 const app = express();
+
+async function getData() {
+  try {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${API_KEY}`,
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}...`));
 
