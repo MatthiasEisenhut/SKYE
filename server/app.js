1ab56c4f453008132ef81e24c278251ac51e7b26
MatthiasEisenhut <eisenhu.m03@htlwienwest.at>
2021-11-29 18:27:14 +0100

    29.11.2021, 18:27

--- server/app.js
+++ server/app.js
@@ -4,12 +4,16 @@ const axios = require('axios');
 
 require('dotenv').config();
 
+const weatherRoute = require('./routes/data');
+
 const PORT = process.env.PORT || 5000;
 const API_KEY = process.env.API_KEY;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json()); // body parser
+app.use('/weather', weatherRoute);
 
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}...`));
 
