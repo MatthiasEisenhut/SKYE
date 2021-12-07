6f99cfe1269f3ea2a7687d6ac9b7e31ff8748fd4
MatthiasEisenhut <eisenhut.m03@htlwienwest.at>
2021-12-07 11:16:01 +0100

    test

--- server/controllers/weather.js
+++ server/controllers/weather.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { all } = require('../routes/weather');
 const API_KEY = process.env.API_KEY;
 
 async function getData() {
@@ -6,7 +7,7 @@ async function getData() {
     const { data } = await axios.get(
       `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${API_KEY}`,
     );
-    console.log(data);
+    console.log(data.list[0].wind);
     return data;
   } catch (error) {
     console.log(error);
