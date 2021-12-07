205fb73954f3b0e2214b829318cad429db571bb3
MatthiasEisenhut <eisenhut.m03@htlwienwest.at>
2021-12-07 12:32:02 +0100

    daten im backend angezeigt

--- server/routes/weather.js
+++ server/routes/weather.js
@@ -6,18 +6,15 @@ const API_KEY = process.env.API_KEY;
 const { getData } = require('../controllers/weather');
 
 router.get('/getData', async (req, res) => {
-  // const { data } = await res.json(getData());
-  // console.log(data)
-  // res.status(200).send(data);
-   try {
-     const { data } = await axios.get(
-       `https://api.openweathermap.org/data/2.5/onecall?lat=48.2085&lon=16.3721&exclude=minutely,hourly,alerts&units=metric&appid=${API_KEY}`,
-     );
-     console.log(data);
-     res.status(200).send(data);
-   } catch (error) {
-     console.log(error);
-   }
+  try {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=48.2085&lon=16.3721&exclude=minutely,hourly,alerts&units=metric&appid=${API_KEY}`,
+    );
+    console.log(data);
+    res.status(200).send(data);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
