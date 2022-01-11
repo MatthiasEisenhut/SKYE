d10e1ed2df768b18ebf47b1a6f813785f0403b50
MatthiasEisenhut <eisenhut.m03@htlwienwest.at>
2022-01-11 12:47:26 +0100

    Suche fertig

--- client/js/app.js
+++ client/js/app.js
@@ -23,10 +23,11 @@ const myApp = {
       imagefive: '',
       imagesix: '',
       windSpeed: 0,
+      city: 'London',
     };
   },
   async created() {
-    const { data } = await axios.get('http://localhost:3000/weather/getData');
+    const { data } = await axios.get(`http://localhost:3000/weather/getData/${this.city}`);
     this.weather = data;
 
     const directions = ['N', 'N-E', 'E', 'S-E', 'S', 'S-W', 'W', 'N-W  '];
@@ -111,6 +112,106 @@ const myApp = {
     this.imagefive = `http://openweathermap.org/img/wn/${this.weather.daily[4].weather[0].icon}@2x.png`;
     this.imagesix = `http://openweathermap.org/img/wn/${this.weather.daily[5].weather[0].icon}@2x.png`;
   },
+  methods: {
+    async searchMethod() {
+      const { data } = await axios.get(`http://localhost:3000/weather/getData/${this.city}`);
+      this.weather = data;
+
+      const directions = ['N', 'N-E', 'E', 'S-E', 'S', 'S-W', 'W', 'N-W  '];
+      const weekday = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+      ];
+      const month = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ];
+
+      const today = new Date();
+      const secondday = new Date(this.weather.daily[1].dt * 1000);
+      const thirdday = new Date(this.weather.daily[2].dt * 1000);
+      const fourthday = new Date(this.weather.daily[3].dt * 1000);
+      const fifthday = new Date(this.weather.daily[4].dt * 1000);
+      const sixthday = new Date(this.weather.daily[5].dt * 1000);
+      const rise = new Date(this.weather.current.sunrise * 1000);
+      const down = new Date(this.weather.current.sunset * 1000);
+
+      this.sunrise =
+        rise.getHours() +
+        ':' +
+        rise.getMinutes().toLocaleString('en-US', {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        });
+
+      this.sundown =
+        down.getHours() +
+        ':' +
+        down.getMinutes().toLocaleString('en-US', {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        });
+
+      this.date =
+        today.getHours() +
+        ':' +
+        today.getMinutes().toLocaleString('en-US', {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        });
+
+      if (today.getHours() <= 12) {
+        this.ampm = 'AM';
+      } else {
+        this.ampm = 'PM';
+      }
+
+      this.currentdatetime =
+        weekday[today.getDay()] + ', ' + today.getDate() + ' ' + month[today.getMonth()];
+
+      this.one = weekday[today.getDay()];
+      this.two = weekday[secondday.getDay()].substring(0, 3);
+      this.three = weekday[thirdday.getDay()].substring(0, 3);
+      this.four = weekday[fourthday.getDay()].substring(0, 3);
+      this.five = weekday[fifthday.getDay()].substring(0, 3);
+      this.six = weekday[sixthday.getDay()].substring(0, 3);
+
+      const index =
+        Math.round(
+          ((this.weather.current.wind_deg %= 360) < 0
+            ? this.weather.current.wind_deg + 360
+            : this.weather.current.wind_deg) / 45,
+        ) % 8;
+      this.dir = directions[index];
+      this.windSpeed = this.weather.current.wind_speed.toFixed(2);
+
+      this.imagetoday = `http://openweathermap.org/img/wn/${this.weather.current.weather[0].icon}@2x.png`;
+      this.imagetwo = `http://openweathermap.org/img/wn/${this.weather.daily[1].weather[0].icon}@2x.png`;
+      this.imagethree = `http://openweathermap.org/img/wn/${this.weather.daily[2].weather[0].icon}@2x.png`;
+      this.imagefour = `http://openweathermap.org/img/wn/${this.weather.daily[3].weather[0].icon}@2x.png`;
+      this.imagefive = `http://openweathermap.org/img/wn/${this.weather.daily[4].weather[0].icon}@2x.png`;
+      this.imagesix = `http://openweathermap.org/img/wn/${this.weather.daily[5].weather[0].icon}@2x.png`;
+    },
+  },
 };
 
 createApp(myApp).mount('#app');
+
+module.exports = {
+  city,
+};
--- server/routes/weather.js
+++ server/routes/weather.js
@@ -1,17 +1,28 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const axios = require("axios");
+const axios = require('axios');
 const API_KEY = process.env.API_KEY;
 
-router.get("/getData", async (req, res) => {
+router.get('/getData/:city', async (req, res) => {
+  const { city } = req.params;
+  const cityCoords = await getCity(city);
+
   try {
     const { data } = await axios.get(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=48.2085&lon=16.3721&exclude=minutely,hourly,alerts&units=metric&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${cityCoords[0].lat}&lon=${cityCoords[0].lon}&exclude=minutely,hourly,alerts&units=metric&appid=${API_KEY}`,
     );
     res.status(200).send(data);
   } catch (error) {
     console.log(error);
   }
 });
+
+async function getCity(city) {
+  const { data } = await axios.get(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`,
+  );
+
+  return data;
+}
 
 module.exports = router;
