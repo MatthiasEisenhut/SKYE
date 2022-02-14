73a40b6131993c5b3a5864a78bf50e7381a8fe83
MatthiasEisenhut <eisenhut.m03@htlwienwest.at>
2022-02-14 15:54:00 +0100

    Code bereinigt

--- server/app.js
+++ server/app.js
@@ -10,9 +10,9 @@ const weatherRoute = require('./routes/weather');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json()); // body parser
+app.use(express.json());
 app.use('/weather', weatherRoute);
 
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}...`));
